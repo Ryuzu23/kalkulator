59f8f9a5a3f046d064825e1a38269587f3c9f80a
Ari Ardana <62498753+Ryuzu23@users.noreply.github.com>
2020-04-13 14:26:27 +0700

    Update kalkulator.py

--- kalkulator.py
+++ kalkulator.py
@@ -6,19 +6,19 @@ print ('#        https://github.com/Ryuzu23       #')
 print ('###########################################\n')
 
 def tambah(a, b):
-	print ('Jawabannya adalah: \n')
+	print ('\nJawabannya adalah: \n')
 	print (a, '+',b, '=',a + b)
 	return
 def kurang(c, d):
-	print ('Jawabannya adalah: \n')
+	print ('\nJawabannya adalah: \n')
 	print (c, '-',d, '=',c - d)
 	return
 def kali(e, f):
-	print ('Jawabannya adalah: \n')
+	print ('\nJawabannya adalah: \n')
 	print (e, 'x',f, '=',e * f)
 	return
 def bagi(g, h):
-	print ('Jawabannya adalah: \n')
+	print ('\nJawabannya adalah: \n')
 	print (g, ':',h, '=',g / h)
 	return
 
@@ -34,23 +34,23 @@ def menu():
 			print ('3. Perkalian')
 			print ('4. Pembagian')
 			print ('5. Keluar\n')
-			pilih = input('Pilih angka 1, 2, 3, 4,atau 5 lalu tekan Enter: \n')
+			pilih = input('Pilih angka 1, 2, 3, 4,atau 5 lalu tekan Enter: ')
 
 			if pilih == '1':
 				a = float(input('Angka: '))
-				b = float(input('Ditambah dengan angka: \n'))
+				b = float(input('Ditambah dengan angka: '))
 				tambah(a, b)
 			elif pilih == '2':
 				c = float(input('Angka: '))
-				d = float(input('Dikurangi dengan angka: \n'))
+				d = float(input('Dikurangi dengan angka: '))
 				kurang(c, d)
 			elif pilih == '3':
 				e = float(input('Angka: '))
-				f = float(input('Dikali dengan angka: \n'))
+				f = float(input('Dikali dengan angka: '))
 				kali(e, f)
 			elif pilih == '4':
 				g = float(input('Angka: '))
-				h = float(input('Dibagi dengan angka: \n'))
+				h = float(input('Dibagi dengan angka: '))
 				bagi(g, h)
 			elif pilih == '5':
 				quit = True
